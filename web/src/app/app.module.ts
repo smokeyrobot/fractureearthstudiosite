690a40b47b4cd2c528682b5f7291812c9ec60f09
digitalbooty <50932481+digitalbooty@users.noreply.github.com>
2020-08-30 21:38:43 -0400

    All note fixes from PR comments.

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MissionStatementComponent } from './mission-statement/mission-statement.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { MissionStatementComponent } from './mission-statement/mission-statement
     MissionStatementComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
